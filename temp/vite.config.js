1d27667232794c8c6d5588f99e1f47e808ef17bd
segward <gustavhaverstad@gmail.com>
2026-02-14 13:51:27 +0100

    remade docker shit

--- temp/vite.config.js
+++ temp/vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: true,
+    port: 3000,
     proxy: {
-      "/api": {
-        target: "http://localhost:8080",
+      '/api': {
+        target: 'http://backend:8080',
         changeOrigin: true,
-        secure: false,
       },
     },
   },
-});
+})
